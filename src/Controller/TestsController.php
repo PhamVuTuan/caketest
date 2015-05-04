a8d3dcbe2470c805c335f188642e948571ed2714
tuancopy <tuanphamvu90@gmail.com>
2015-05-04 13:36:36 +0700

    test day file

---
+++ src/Controller/TestsController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+
+class TestsController extends AppController
+{
+    public function view()
+    {
+        $arr = array(1,2,3,4);
+
+        $this->set('data',$arr);
+    }
+
+}
---
+++ src/Template/Tests/view.ctp
@@ -0,0 +1,3 @@
+<h1><?php echo __FILE__ ?></h1>
+<?php
+ debug($data);
